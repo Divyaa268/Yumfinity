aede09c658360a613f34d5b0abb10f28cf075376
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-11 17:21:58 +0530

    Added Restaurant cards JSX
    Added Restaurant cards JSX

--- app.js
+++ app.js
@@ -19,10 +19,19 @@ const Header = ()=>{
 )
 };
 
+// JS Object
+const styleCard = {
+    backgroundColor: "#f0f0f0"
+}
+
 const RestaurantCards = () => {
     return (
-        <div className="res-cards">
+        <div className="res-cards" style={styleCard}> 
+            <img className ="res-logo" alt="res-logo" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/uws6wm1xndcgtox6moif" />
             <h3>Tunday</h3>
+            <h4>Biryani, Kebabs</h4>
+            <h4>4.8 Stars</h4>
+            <h4>28 minutes</h4>
         </div>
     )
 };
