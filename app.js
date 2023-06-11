232287890aed4b1a8e136d488e32667090d2fa18
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-11 15:11:17 +0530

    updated app.js
    updated app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div>
+                <img src ="" />
+            </div>
+            <div className ="nav-items">
+
+            </div>
+        </div>
+    );
+};
+
+const AppLayout = () => {
+    return (
+        <div class="app">
+            // header
+            // body
+            // footer
+
+        </div>
+    )
+}
