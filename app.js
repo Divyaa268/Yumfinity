f7e193cf2144476d3f675438e6b4dd8d507e2980
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-11 16:22:39 +0530

    Updated JS, HTML, installed parcel, react, react-dom
    Updated JS, HTML, installed parcel, react, react-dome

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img className="logo" src ="https://png.pngtree.com/element_our/png_detail/20180913/food-point-logo-design-spoon-fork-and-pin-sign-logo-png_94919.jpg" />
-            </div>
-            <div className ="nav-items">
+const Header = ()=>{
+    return (<div className="header">
+        <div className="logo-container">
+            <img className="logo" src ="https://png.pngtree.com/element_our/png_detail/20180913/food-point-logo-design-spoon-fork-and-pin-sign-logo-png_94919.jpg" />
+        </div>
+        <div className ="nav-items">
                 <ul>
                     <li>Home</li>
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
                 </ul>
-
-            </div>
         </div>
-    );
+    </div>
+)
 };
 
-const AppLayout = () => {
-    return (
-        <div class="app">
-            // header
-            // body
-            // footer
-
+const AppLayout = () =>{
+    return (<div className="app">
+            <Header/>
         </div>
     )
 }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
