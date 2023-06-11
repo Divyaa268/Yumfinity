c66aa468df02ed27e34888edb00f5e1b0588c463
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-11 15:16:22 +0530

    Added Header.
    Added Header.

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
 const Header = () => {
     return (
         <div className="header">
-            <div>
-                <img src ="" />
+            <div className="logo-container">
+                <img className="logo" src ="https://png.pngtree.com/element_our/png_detail/20180913/food-point-logo-design-spoon-fork-and-pin-sign-logo-png_94919.jpg" />
             </div>
             <div className ="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
 
             </div>
         </div>
