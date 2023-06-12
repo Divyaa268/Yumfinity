038d1512414dc693c1fed04dce8d8fda81e6de19
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-12 21:48:35 +0530

    Separated Header component
    Separated Header component

--- src/app.js
+++ src/app.js
@@ -1,23 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Functional component 
-const Header = ()=>{
-    return (<div className="header">
-        <div className="logo-container">
-            <img className="logo" src ="https://png.pngtree.com/element_our/png_detail/20180913/food-point-logo-design-spoon-fork-and-pin-sign-logo-png_94919.jpg" />
-        </div>
-        <div className ="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-        </div>
-    </div>
-)
-};
+
 
 // JS Object
 const styleCard = {
