db5c6f66dd8f319fe7d1d6ea6cf7ffff5d676d8e
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-12 15:30:24 +0530

    Cleaned code using optional chaining
    Cleaned code using optional chaining

--- app.js
+++ app.js
@@ -27,15 +27,23 @@ const styleCard = {
 const RestaurantCards = (props) => {
     const {resData} = props;
 
+    // optional chaining ?.
+    const {cloudinaryImageId, name, avgRating, deliveryTime, cuisines  } = resData?.data;
+
     return (
         <div className="res-cards" style={styleCard}> 
-            <img className ="res-logo" alt="res-logo" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" + resData.data.cloudinaryImageId} />
+            <img className ="res-logo" alt="res-logo" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" + cloudinaryImageId} />
             {/* <h3>{props.resName}</h3>
             <h4>{props.cuisine}</h4> */}
-            <h3>{resData.data.name}</h3>
+            {/* <h3>{resData.data.name}</h3>
             <h4>{resData.data.avgRating}</h4>
             <h4>{resData.data.deliveryTime + " minutes"}</h4>
-            <h4>{resData.data.cuisines.join(", ")}</h4>
+            <h4>{resData.data.cuisines.join(", ")}</h4> */}
+
+            <h3>{name}</h3>
+            <h4>{avgRating}</h4>
+            <h4>{deliveryTime + " minutes"}</h4>
+            <h4>{cuisines.join(", ")}</h4>
         </div>
     )
 };
