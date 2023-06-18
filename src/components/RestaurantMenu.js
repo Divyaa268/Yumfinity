fcf5e0270365f82f1f16d2d503bfe143c103920b
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-19 01:43:34 +0530

    Created fetchMenu functions
    Created fetchMenu functions

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
+
 const RestaurantMenu = () => {
+
+    useEffect( () => {
+        fetchMenu();
+    }, []);
+
+    const fetchMenu = async () => 
+    {
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8541536&lng=80.94478269999999&restaurantId=635227&submitAction=ENTER");
+        const json = data.json();
+
+        console.log(json);
+    }
     return(
         <div className="menu">
             <h1>Restaurant Name</h1>
