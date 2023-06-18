765327cca9db3eaf20d7481fc256787a38ff43fb
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-18 17:21:12 +0530

    imported outlet and added children routes
    imported outlet and added children routes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import RestaurantCards from "./components/RestaurantCards";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
@@ -146,7 +146,7 @@ import Error from "./components/Error";
 const AppLayout = () =>{
     return (<div className="app">
             <Header/>
-            <Body />
+            <Outlet />
         </div>
     )
 }
@@ -156,18 +156,26 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />
+      },
+      {
+        path: "/about",
+        element: <About />
+        },
+        {
+          path: "/contact",
+          element: <Contact />
+        },
+
+    ],
     errorElement: <Error />
 
   },
-  {
-  path: "/about",
-  element: <About />
-  },
-  {
-    path: "/contact",
-    element: <Contact />
-  },
-])
+  
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
