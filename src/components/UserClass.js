07c9bb2afebc1fc0c386424d6f55c947ff2fd224
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-23 03:46:37 +0530

    Using state variable in Class Component
    Using state variable in Class Component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -7,14 +7,23 @@ class UserClass extends React.Component
         super(props);
 
     console.log(props);
+
+    // state variable
+    this.state = {
+        count: 0,
+    }
     }
 
     render() {
         // destructuring -
         // const { name, location } = this.props;
+
+        // destructuring state variable -
+        // const { count } = this.state;
         return(
             <div className="user-card">
             {/* <h2>Name: {name}</h2>  */}
+            <h1> Count: {this.state.count}</h1>
             <h2>Name: {this.props.name}</h2>
             <h3>Location: {this.props.location}</h3>
             <h4>Contact: divyaad248@gmail.com</h4>
