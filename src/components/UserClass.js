db0d5c52512b9be0c00cf8eebdb87b09034fc82a
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-23 03:09:22 +0530

    Updated UserClass
    Updated UserClass

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,5 +1,17 @@
+import React from "react";
 // Class Component 
 class UserClass extends React.Component
 {
-    
+    render() {
+        return(
+            <div className="user-card">
+            <h2>Name: Divya</h2>
+            <h3>Location: Lucknow</h3>
+            <h4>Contact: divyaad248@gmail.com</h4>
+
+        </div>
+        )
+    }
 }
+
+export default UserClass;
