24dd60ed04cd4ee5c5bf2b3496c0fb289abf8f30
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-23 03:38:38 +0530

    Using useState variable
    Using useState variable

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 // Functional Component
 const User = (props) => {
+// const User = ({name}) => {
+
+    const [count] = useState(0);
 
     return(
         <div className="user-card">
+            <h1>Count = {count}</h1>
             <h2>Name: {props.name}</h2>
             <h3>Location: Lucknow</h3>
             <h4>Contact: divyaad248@gmail.com</h4>
