eacec25bf3b2aad5ecbfb6d968b88383e52304b8
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-23 02:50:52 +0530

    Updated and exported/imported User component.
    Updated and exported/imported User component.

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,13 @@
 const User = () => {
 
     return(
-        <div>
-            
+        <div className="user-card">
+            <h2>Name: Divya</h2>
+            <h3>Location: Lucknow</h3>
+            <h4>Contact: divyaad248@gmail.com</h4>
+
         </div>
     )
 }
+
+export default User;
