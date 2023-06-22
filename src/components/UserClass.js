ab8667ed49de28efb5b8cfebfaba1a71a50980c9
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-06-23 03:27:36 +0530

    Using constructor to introduce props
    Using constructor to introduce props

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,11 +2,18 @@ import React from "react";
 // Class Component 
 class UserClass extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+
+    console.log(props);
+    }
+
     render() {
         return(
             <div className="user-card">
-            <h2>Name: Divya</h2>
-            <h3>Location: Lucknow</h3>
+            <h2>Name: {this.props.name}</h2>
+            <h3>Location: {this.props.location}</h3>
             <h4>Contact: divyaad248@gmail.com</h4>
 
         </div>
