0792a99590a4def36f298fd68b5ddfeab853d6c5
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-05 13:40:49 -0400

    updating the state variable
    updating the state variable

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -25,7 +25,18 @@ class UserClass extends React.Component
         return(
             <div className="user-card">
             {/* <h2>Name: {name}</h2>  */}
+
+
+            {/* Updating the Count var */}
             <h1> Count: {this.state.count}</h1>
+            <button onClick={() => {
+                this.setState({
+                    count: this.state.count + 1,
+                    count2: this.state.count2 +1,
+                })
+
+            }}>Count Increases</button>
+
             <h1> Count2: {this.state.count2}</h1>
             {/* <h2>Name: {this.props.name}</h2>
             <h3>Location: {this.props.location}</h3> */}
