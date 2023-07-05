21f406a72fb103578c212fd54fd72d8f423fa4a1
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-05 13:53:27 -0400

    Changed About from Functional to Class Component
    Changed About from Functional to Class Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,44 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import React from "react";
 
-const About = () => {
-    return(
-        <div>
-            <h1> Welcome to About Us page</h1>
-            <User name={"Divya from Functional Component"}/>
+// Functional
+// const About = () => {
+//     return(
+//         <div>
+//             <h1> Welcome to About Us page</h1>
+//             <User name={"Divya from Functional Component"}/>
 
-            <UserClass name={"Divya from Class Component"} location={"Lucknow Class"} />
-        </div>
-    )
+//             <UserClass name={"Divya from Class Component"} location={"Lucknow Class"} />
+//         </div>
+//     )
+// }
+
+// export default About;
+
+
+class About extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+
+        console.log("Parent Constructor");
+    }
+
+    render() 
+    {
+        console.log("Render()")
+            return(
+                <div>
+                    <h1> Welcome to About Us page</h1>
+                    <User name={"Divya from Functional Component"}/>
+        
+                    <UserClass name={"Divya from Class Component"} location={"Lucknow Class"} />
+                </div>
+            )
+        }
+
 }
-
+ 
 export default About;
