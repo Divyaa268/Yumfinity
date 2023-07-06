f9da034ad725e34d9b6abea6682beeb32c70203f
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-06 12:35:29 -0400

    Added online status symbol in header
    Added online status symbol in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 // Functional component 
 const Header = ()=>{
 
     const [ btnNameReact, setBtnNameReact ] = useState("Login");
+
+    const onlineStatus = useOnlineStatus();
 
     return (<div className="header">
         <div className="logo-container">
@@ -13,6 +16,9 @@ const Header = ()=>{
         </div>
         <div className ="nav-items">
                 <ul>
+                    <li> 
+                        Online Status: {onlineStatus? "✅" : "🔴" }
+                    </li>
                     <li> 
                         <Link to="/">Home</Link>
                     </li>
