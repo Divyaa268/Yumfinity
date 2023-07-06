d93c5cb8fb157efa4cd9c96c70cfde761e8936a3
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-05 21:36:44 -0400

    Added state variable and fetched and updated data after API Call
    Added state variable and fetched and updated data after API Call

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,7 +1,25 @@
-
+import { useEffect, useState } from "react";
 // takes resId and gives restInfo
-const useRestaurantMenu = (resId) => 
+const useRestaurantMenu  = (resId) => 
 {
+    const [restInfo, setRestInfo] = useState(null); // using state variable to change value after api call
+
+    // fetch data
+    useEffect(() => {
+        fetchData();
+    }, []); // keeping [] empty as we want to fetch data only once
+
+     
+    const fetchData = async () => 
+    {
+        const data = await fetch();
+        const json = await data.json();
+
+        setRestInfo(json); // updating with data fetched from API
+    }
+
     return restInfo;
 
 }
+
+export default useRestaurantMenu;
