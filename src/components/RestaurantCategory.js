03a812dcdc439ec0ab4c9dd1a3ef32eb32425819
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-12 15:43:03 -0400

    Added length of items for each category
    Added length of items for each category

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -7,7 +7,7 @@ const RestaurantCategory = ({data}) => {
         <div>
             {/* header */}
             <div className="w-6/12 mx-auto my-4 p-4 bg-emerald-100 shadow-lg hover:bg-emerald-50 flex justify-between font-bold ">
-                <span>{data.title}</span>
+                <span>{data.title} ({data.itemCards.length})</span>
                 <span>⬇️</span>
             </div>
             {/* Accordian menu */}
