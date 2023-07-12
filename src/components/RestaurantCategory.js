63f049b96c5a1359aa6da8b41d04f6733bd0dc9e
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-12 15:08:03 -0400

    Created component Restaurant category

---
+++ src/components/RestaurantCategory.js
@@ -0,0 +1,15 @@
+//  receiving data here from RestaurantMenu where we created categories.map
+const RestaurantCategory = ({data}) => { 
+
+    console.log(data)
+
+    return (
+        <div>
+            {/* header */}
+            Category
+            {/* Accordian menu */}
+        </div>
+    )
+}
+
+export default RestaurantCategory;
