d9c7f14950c86e8c13273e24dd695053592b3c15
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-12 16:01:12 -0400

    Added tailwind and data
    Added tailwind and data

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,3 +1,5 @@
+import ItemList from "./ItemList";
+
 //  receiving data here from RestaurantMenu where we created categories.map
 const RestaurantCategory = ({data}) => { 
 
@@ -6,11 +8,15 @@ const RestaurantCategory = ({data}) => {
     return (
         <div>
             {/* header */}
-            <div className="w-6/12 mx-auto my-4 p-4 bg-emerald-100 shadow-lg hover:bg-emerald-50 flex justify-between font-bold ">
-                <span>{data.title} ({data.itemCards.length})</span>
-                <span>⬇️</span>
+            <div className="w-6/12 mx-auto my-4 p-4 bg-emerald-100 shadow-lg hover:bg-emerald-50 ">
+                <div className="flex justify-between  font-bold">
+                    <span>{data.title} ({data.itemCards.length})</span>
+                    <span>⬇️</span>
+                </div>
+
+                 {/* Accordian body menu */}
+            <ItemList items={ data.itemCards } />
             </div>
-            {/* Accordian menu */}
         </div>
     )
 }
