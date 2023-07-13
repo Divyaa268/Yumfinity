ca6d7c0c28e15aeca2a1b4022ed77205eeb4adb2
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-13 17:14:44 -0400

    Added useState for expanding and minimizing accordian
    Added useState for expanding and minimizing accordian

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,21 +1,32 @@
+// import { useState } from "react";
+import { useState } from "react";
 import ItemList from "./ItemList";
 
 //  receiving data here from RestaurantMenu where we created categories.map
 const RestaurantCategory = ({data}) => { 
 
-    console.log(data)
+    // console.log(data)
+
+    const [ showItems, setShowItems ] = useState(false);
+
+    const handleClick = () => {
+
+        setShowItems(!showItems);
+
+    }
 
     return (
         <div>
             {/* header */}
             <div className="w-6/12 mx-auto my-4 p-4 bg-emerald-100 shadow-lg hover:bg-emerald-50 ">
-                <div className="flex justify-between  font-bold">
+                <div className="flex justify-between  font-bold cursor-pointer" 
+                onClick={handleClick}>
                     <span>{data.title} ({data.itemCards.length})</span>
                     <span>⬇️</span>
                 </div>
 
                  {/* Accordian body menu */}
-            <ItemList items={ data.itemCards } />
+                { showItems && <ItemList items={ data.itemCards } /> }
             </div>
         </div>
     )
