cdfda1d2cc9da4fd80b17193569115252e5922c7
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-13 15:27:29 -0400

    Updated tailwind for ItemList
    Updated tailwind for ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,3 +1,43 @@
+// import { CDN_URL } from "../utils/constants";
+
+// const ItemList = ({ items }) =>
+// {
+//     console.log(items);
+
+//     return(
+//         <div>
+//             {items.map((item) => (
+//                 <div key={item.card.info.id} className="p-2 m-2 border-slate-300 border-b-2 text-left flex justify-between">
+                    
+//                     <div className="p-4">
+//                     <img src={CDN_URL + item.card.info.imageId}  className="h-auto max-w-lg"/>
+//                     </div>
+                    
+//                     <div>
+//                     <div className="py-2">
+//                         <span className="p-2 font-semibold">{item.card.info.name}</span>
+//                         <span className="p-2 ">₹{ item.card.info.price ? ((item.card.info.price)/100) : ((item.card.info.defaultPrice)/100)}</span>
+                        
+//                     </div>
+//                     <p className="font-light text-xs m-2">{item.card.info.description}</p>
+//                     </div>
+
+                    
+
+                    
+//                 </div>
+                
+//             ))}
+//         </div>
+//     )
+// }
+
+// export default ItemList;
+
+// ---------------------------------------------------
+
+import { CDN_URL } from "../utils/constants";
+
 const ItemList = ({ items }) =>
 {
     console.log(items);
@@ -5,16 +45,28 @@ const ItemList = ({ items }) =>
     return(
         <div>
             {items.map((item) => (
-                <div key={item.card.info.id} className="p-2 m-2 border-slate-300 border-b-2 text-left">
-                    <div className="py-2">
-                        <span className="p-2">{item.card.info.name}</span>
-                        <span className="p-2 ">₹{ item.card.info.price ? ((item.card.info.price)/100) : ((item.card.info.defaultPrice)/100)}</span>
+                <div key={item.card.info.id} className="p-2 m-2 border-slate-300 border-b-2 text-left flex justify-between">
+                    
+                    <div className="w-9/12">
+                        <div className="py-2">
+                            <span className="p-2 font-semibold">{item.card.info.name}</span>
+                            <span className="p-2 ">₹{ item.card.info.price ? ((item.card.info.price)/100) : ((item.card.info.defaultPrice)/100)}</span>
+                        
+                        </div>
+
+                        <p className="font-light text-xs m-2">{item.card.info.description}</p>
                     </div>
-                    <p className="font-light text-xs m-3">{item.card.info.description}</p>
+
+                    
+                    
+                    <div className=" w-3/12 p-4">
+                    <img src={CDN_URL + item.card.info.imageId}  className="w-full"/>
+                    </div>  
                 </div>
+                
             ))}
         </div>
     )
-}
+};
 
 export default ItemList;
