284f42b2e0686ebe9cbfd7eed043da468c0bc582
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-07 00:12:46 -0400

    Added test case to check component rendering
    Added test case to check component rendering

---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import Contact from "../Contact";
+
+
+test("Should load Contact Us Component", () => {
+
+    render(<Contact />)
+
+    // check whether the heading is rendered correctly or not
+
+})
