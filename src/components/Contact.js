e7fcafef77865c02304c1eb480956ac057f540aa
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-06 21:29:07 -0400

    Added styling and form to contact us page
    Added styling and form to contact us page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,14 @@ const Contact = () =>
 {
     return(
         <div>
-            <h1> Contact Us Page</h1>
+            <h1 className="font-bold m-2 p-2 text-3xl"> Contact Us Page</h1>
+
+            <form>
+                <input type="text" className="border-blue-400 p-2 m-2" placeholder="Name"></input>
+                <input type="text" className="border-blue-400 p-2 m-2" placeholder="message"></input>
+
+                <button className="bg-red-700 text-white rounded-md">Submit</button>
+            </form>
         </div>
     )
 }
