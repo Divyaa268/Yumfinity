878fea750e29139eda8c56fff0a0f4d2dadf7a3f
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-11 00:09:01 -0400

    Added test case for login button
    Added test case for login button

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,8 +1,9 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import Header from "../Header"
 import { Provider } from "react-redux"
 import appStore from "../../utils/appStore"
 import { BrowserRouter } from "react-router-dom"
+import "@testing-library/jest-dom";
 
 it("Should load the login button in Header component", () => {
 
@@ -13,5 +14,10 @@ it("Should load the login button in Header component", () => {
     <Header />
     </Provider>
     </BrowserRouter>
-    )
+    );
+
+    // const loginButton = screen.getByRole("button");
+    const loginButton = screen.getByText("Login");
+
+    expect(loginButton).toBeInTheDocument();
 })
