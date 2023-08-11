eefb1a1d0aefeb5b1b689defddff58ecd757dd94
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-11 01:23:57 -0400

    Created a test case file RestaurantCard component
    Created a test case file RestaurantCard component

--- src/components/__tests__/RestaurantCard.test.js
+++ src/components/__tests__/RestaurantCard.test.js
@@ -1,2 +1,7 @@
+import { render } from "@testing-library/react"
+import RestaurantCards from "../RestaurantCards"
 
-it("Should render RestaurantCard component with Data (props)")
+it("Should render RestaurantCard component with Data (props)", () => {
+
+    render(<RestaurantCards />)
+})
