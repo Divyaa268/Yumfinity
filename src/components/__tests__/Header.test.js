8bb72c4cc73dbac4686c19d45a2b72f92a06e198
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-11 01:18:56 -0400

    Added test case for click event
    Added test case for click event

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Header from "../Header"
 import { Provider } from "react-redux"
 import appStore from "../../utils/appStore"
@@ -41,6 +41,26 @@ it("Should load the cart items in Header component", () => {
     // const cartItems = screen.getByText("Cart (0)");  // in case of multiple buttons
     const cartItems = screen.getByText(/Cart/);  // using regex
     
-
     expect(cartItems).toBeInTheDocument();
+})
+
+
+it("Should change login button to logout in Header component", () => {
+
+    render(
+    // store will come from appStore
+    <BrowserRouter>
+    <Provider store={appStore}>
+    <Header />
+    </Provider>
+    </BrowserRouter>
+    );
+
+    const loginButton = screen.getByRole("button", { name:"Login" }); 
+
+    fireEvent.click(loginButton);
+
+    const logoutButton = screen.getByRole("button", { name:"Logout" }); 
+
+    expect(logoutButton).toBeInTheDocument();
 })
