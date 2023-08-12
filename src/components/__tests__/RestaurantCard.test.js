3e56875df1b6f8e3fe3ac9583c6c2c963352d179
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-12 17:01:45 -0400

    Added test case for Search Button
    Added test case for Search Button

--- src/components/__tests__/RestaurantCard.test.js
+++ src/components/__tests__/RestaurantCard.test.js
@@ -12,3 +12,14 @@ it("Should render RestaurantCard component with Data (props)", () => {
 
     expect(restName).toBeInTheDocument();
 })
+
+
+// it("Should render RestaurantCard component with Promoted Label", () => {
+
+//     // as restaurantCard component has a prop
+//     render(<RestaurantCards resData={MOCK_DATA} />)
+
+//     const restName = screen.getByText("Tunday Kababi (Aminabad)")
+
+//     expect(restName).toBeInTheDocument();
+// })
---
+++ src/components/__tests__/Search.test.js
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react"
+import Body from "../Body"
+
+it("Should render Serach button on Body component", () => {
+
+    render(<Body />);
+})
