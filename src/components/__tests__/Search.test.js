a8eca976c58689dbcf205036b1ffc01cc66989b9
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-12 22:55:22 -0400

    Added mock fetch function
    Added mock fetch function

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -1,6 +1,18 @@
 import { render } from "@testing-library/react"
 import Body from "../Body"
 
+// here the dummy fetch function is exactly like the fetch which the browser gives us
+// global is a global object // defining our own dummy function for fetch which will return a promise 
+// fetch function will resolve with a json
+// json is a function that returns a Promise.resolve which has data
+global.fetch = jest.fn( () => {
+    return Promise.resolve({
+        json: () => {
+            return Promise.resolve(data);
+        }
+    })
+})
+
 it("Should render Serach button on Body component", () => {
 
     render(<Body />);
